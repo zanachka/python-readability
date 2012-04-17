b498df200b3208d285a6dd91f1ae4a92fa16f7ce
Richard Harding <rharding@mitechie.com>
2012-04-17 14:14:02 -0400

    More pep8, almost there

--- readability/htmls.py
+++ readability/htmls.py
@@ -1,13 +1,17 @@
-from cleaners import normalize_spaces, clean_attributes
-from encoding import get_encoding
-from lxml.html import tostring
 import logging
-import lxml.html
 import re
 
-logging.getLogger().setLevel(logging.DEBUG)
+from lxml.html import document_fromstring
+from lxml.html import HTMLParser
+from lxml.html import tostring
 
-utf8_parser = lxml.html.HTMLParser(encoding='utf-8')
+from cleaners import clean_attributes
+from cleaners import normalize_spaces
+from encoding import get_encoding
+
+logging.getLogger().setLevel(logging.DEBUG)
+utf8_parser = HTMLParser(encoding='utf-8')
+
 
 def build_doc(page):
     if isinstance(page, unicode):
@@ -15,8 +19,11 @@ def build_doc(page):
     else:
         enc = get_encoding(page)
         page_unicode = page.decode(enc, 'replace')
-    doc = lxml.html.document_fromstring(page_unicode.encode('utf-8', 'replace'), parser=utf8_parser)
+    doc = document_fromstring(
+        page_unicode.encode('utf-8', 'replace'),
+        parser=utf8_parser)
     return doc
+
 
 def js_re(src, pattern, flags, repl):
     return re.compile(pattern, flags).sub(src, repl.replace('$', '\\'))
@@ -24,8 +31,8 @@ def js_re(src, pattern, flags, repl):
 
 def normalize_entities(cur_title):
     entities = {
-        u'\u2014':'-',
-        u'\u2013':'-',
+        u'\u2014': '-',
+        u'\u2013': '-',
         u'&mdash;': '-',
         u'&ndash;': '-',
         u'\u00A0': ' ',
@@ -39,15 +46,18 @@ def normalize_entities(cur_title):
 
     return cur_title
 
+
 def norm_title(title):
     return normalize_entities(normalize_spaces(title))
+
 
 def get_title(doc):
     title = doc.find('.//title').text
     if not title:
         return '[no-title]'
-    
+
     return norm_title(title)
+
 
 def add_match(collection, text, orig):
     text = norm_title(text)
@@ -55,11 +65,12 @@ def add_match(collection, text, orig):
         if text.replace('"', '') in orig.replace('"', ''):
             collection.add(text)
 
+
 def shorten_title(doc):
     title = doc.find('.//title').text
     if not title:
         return ''
-    
+
     title = orig = norm_title(title)
 
     candidates = set()
@@ -71,13 +82,14 @@ def shorten_title(doc):
             if e.text_content():
                 add_match(candidates, e.text_content(), orig)
 
-    for item in ['#title', '#head', '#heading', '.pageTitle', '.news_title', '.title', '.head', '.heading', '.contentheading', '.small_header_red']:
+    for item in ['#title', '#head', '#heading', '.pageTitle', '.news_title',
+        '.title', '.head', '.heading', '.contentheading', '.small_header_red']:
         for e in doc.cssselect(item):
             if e.text:
                 add_match(candidates, e.text, orig)
             if e.text_content():
                 add_match(candidates, e.text_content(), orig)
-                
+
     if candidates:
         title = sorted(candidates, key=len)[-1]
     else:
@@ -103,13 +115,16 @@ def shorten_title(doc):
 
     return title
 
+
 def get_body(doc):
-    [ elem.drop_tree() for elem in doc.xpath('.//script | .//link | .//style') ]
+    [elem.drop_tree() for elem in doc.xpath('.//script | .//link | .//style')]
     raw_html = unicode(tostring(doc.body or doc))
     cleaned = clean_attributes(raw_html)
     try:
         #BeautifulSoup(cleaned) #FIXME do we really need to try loading it?
         return cleaned
-    except Exception: #FIXME find the equivalent lxml error
-        logging.error("cleansing broke html content: %s\n---------\n%s" % (raw_html, cleaned))
+    except Exception:  # FIXME find the equivalent lxml error
+        logging.error("cleansing broke html content: %s\n---------\n%s" % (
+            raw_html,
+            cleaned))
         return raw_html
